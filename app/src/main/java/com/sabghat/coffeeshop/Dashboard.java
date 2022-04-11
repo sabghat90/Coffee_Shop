d1de330d1c2359a38721917a4623a5874fe17478
Sabghat Ullah <sabghat90@gmail.com>
2022-04-11 08:37:47 +0500

    Nothing done haha

--- app/src/main/java/com/sabghat/coffeeshop/Dashboard.java
+++ app/src/main/java/com/sabghat/coffeeshop/Dashboard.java
@@ -1,20 +1,20 @@
 package com.sabghat.coffeeshop;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Dashboard extends AppCompatActivity {
 
     // Variables
-    RecyclerView recyclerView;
-    LinearLayoutManager linearLayoutManager;
-    List<ItemModelClass> itemModelClassList;
+    RecyclerView itemRecyclerView;
+    String[] itemName, itemDescription;
+    int[] imageResource = {R.drawable.image1,R.drawable.coffee_mug,R.drawable.coffee_mug};
+
+    // Objects
     ItemAdapter itemAdapter;
 
     @Override
@@ -22,24 +22,15 @@ public class Dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        initData();
-        initRecyclerView();
-    }
+        itemRecyclerView = findViewById(R.id.recyclerview_card);
+        itemName = getResources().getStringArray(R.array.item_name_array);
+        itemDescription = getResources().getStringArray(R.array.item_description_array);
 
-    private void initData() {
-        itemModelClassList = new ArrayList<>();
-        itemModelClassList.add(new ItemModelClass(R.drawable.image1,"Coffee","Tea is Fantastic"));
-    }
+        itemAdapter = new ItemAdapter(itemName,itemDescription,imageResource,this);
 
-    private void initRecyclerView() {
-
-        recyclerView = findViewById(R.id.recyclerview_card);
-        linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        itemAdapter = new ItemAdapter(itemModelClassList);
-        recyclerView.setAdapter(itemAdapter);
-        itemAdapter.notifyDataSetChanged();
+        itemRecyclerView.setAdapter(itemAdapter);
+        itemRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Log.d("Dashboard","Dashboard finished");
     }
 
 }
--- app/src/main/java/com/sabghat/coffeeshop/ItemAdapter.java
+++ app/src/main/java/com/sabghat/coffeeshop/ItemAdapter.java
@@ -1,5 +1,7 @@
 package com.sabghat.coffeeshop;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,55 +11,52 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
-
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
 
-    private final List<ItemModelClass> itemModelClassList;
+    String[] itemName, itemDescription;
+    int[] imageResource;
+    Context ctx;
 
-    public ItemAdapter(List<ItemModelClass> itemModelClassList) {
-        this.itemModelClassList = itemModelClassList;
+    public ItemAdapter(String[] itemName, String[] itemDescription, int[] imageResource, Context ctx) {
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.imageResource = imageResource;
+        this.ctx = ctx;
+        Log.d("Adapter","Item Adapter ctor finished");
     }
 
     @NonNull
     @Override
     public ItemAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view,parent,false);
+
+        LayoutInflater layoutInflater = LayoutInflater.from(ctx);
+        View view = layoutInflater.inflate(R.layout.item_view,parent,false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        int itemImage = itemModelClassList.get(position).getItemImage();
-        String itemName = itemModelClassList.get(position).getTvItemName();
-        String itemDescription = itemModelClassList.get(position).getTvItemDescription();
-
-        holder.setData(itemImage,itemName,itemDescription);
+        holder.tvItemName.setText(itemName[position]);
+        holder.tvItemDescription.setText(itemDescription[position]);
+        holder.imageView.setImageResource(imageResource[position]);
     }
 
     @Override
     public int getItemCount() {
-        return itemModelClassList.size();
+        return itemName.length;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private ImageView ivItemImage;
-        private TextView tvItemName;
-        private TextView tvItemDescription;
+
+        TextView tvItemName, tvItemDescription;
+        ImageView imageView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            ivItemImage = itemView.findViewById(R.id.item_image);
             tvItemName = itemView.findViewById(R.id.item_name);
             tvItemDescription = itemView.findViewById(R.id.item_description);
-        }
-
-        public void setData(int item_image, String item_name, String item_description) {
-
-            ivItemImage.setImageResource(item_image);
-            tvItemName.setText(item_name);
-            tvItemDescription.setText(item_description);
+            imageView = itemView.findViewById(R.id.item_image);
         }
     }
 }
